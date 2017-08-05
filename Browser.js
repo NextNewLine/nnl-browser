4f5acd8c7c78da3d677c99c4aba18fd474ce57cd
Sausage <wardrox@gmail.com>
2017-08-05 15:40:58 +0100

    clickLink works for elements which arent anchors

--- Browser.js
+++ Browser.js
@@ -102,7 +102,7 @@ module.exports = function(args) {
 			if (selector.indexOf(".") === -1 && selector.indexOf("#") === -1) { //    var possibleButtons = document.querySelectorAll(\"button, input, a.btn\"); var buttonToClick; for (var i = 0; i < possibleButtons.length; i++) { console.debug(possibleButtons[i].innerText); if (possibleButtons[i].innerText == \"" + selector + "\"){ buttonToClick = possibleButtons;}}  if (buttonToClick){ buttonToClick.click(); var href = buttonToClick.getAttribute('href'); if (href && href.length > 1) {window.location.href = href}} else {console.log('No Link found')}
 				script = "function(){ var selector = $(\"a:contains('" + selector + "')\").first(); selector.trigger('click'); if (selector.attr('href') && selector.attr('href').length > 1) { window.location.href = selector.attr('href');} }";
 			} else {
-				script = "function(){ var selector = $(\"a" + selector + "\").first(); selector.trigger('click'); if (selector.attr('href') && selector.attr('href').length) { window.location.href = selector.attr('href');} }";
+				script = "function(){ var selector = $(\"" + selector + "\").first(); selector.trigger('click'); if (selector.attr('href') && selector.attr('href').length) { window.location.href = selector.attr('href');} }";
 			}
 
 			phantomPage.evaluateJavaScript(script).then(() => {
