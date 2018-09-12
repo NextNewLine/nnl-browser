9ef0afaf937d3dcc48a7e8fb2eb0c41f0fb3fda8
John <john@m14.industries>
2018-09-12 11:39:40 +0100

    M14BROWSER_WAITTIMEOUT env var added to overwrite default 400ms wait time

--- Browser.js
+++ Browser.js
@@ -8,7 +8,7 @@ module.exports = function(args) {
 
 	let baseUrl = "http://localhost:3000";
 
-	let waitForRedirection = 400; // how long to wait for a redirection after a button or link has been clicked?
+	let waitForRedirection = Number(process.env.M14BROWSER_WAITTIMEOUT) || 400; // how long to wait for a redirection after a button or link has been clicked?
 
 	let viewportSize = {
 		width: 600,
